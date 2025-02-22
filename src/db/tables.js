b5e6cd0815387767cfaef4909e33c886c9c29eaa
b1gd3vd0g <devin@bigdevdog.com>
2025-02-21 17:30:45 -0800

    make async, add created timestamp

--- src/db/tables.js
+++ src/db/tables.js
@@ -1,13 +1,14 @@
 const sql = require('./sql');
 
-const createUsersTable = () => {
-    sql`
+const createUsersTable = async () => {
+    await sql`
         CREATE TABLE IF NOT EXISTS users(
-            user_id     TEXT    PRIMARY KEY UNIQUE NOT NULL,
-            username    TEXT    UNIQUE NOT NULL,
-            email       TEXT    UNIQUE NOT NULL,
-            password    TEXT    NOT NULL,
-            salt        TEXT    NOT NULL,
+            user_id     TEXT                        PRIMARY KEY UNIQUE NOT NULL,
+            username    TEXT                        UNIQUE NOT NULL,
+            email       TEXT                        UNIQUE NOT NULL,
+            password    TEXT                        NOT NULL,   
+            salt        TEXT                        NOT NULL,
+            created     TIMESTAMP WITH TIME ZONE    NOT NULL
             f_name      TEXT,
             l_name      TEXT,
             job_title   TEXT
