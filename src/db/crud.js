42a1d705b00e1889ac92c0d9265db57ab4f0ca8b
b1gd3vd0g <devin@bigdevdog.com>
2025-02-23 09:43:52 -0800

    improve JSDoc comments.

--- src/db/crud.js
+++ src/db/crud.js
@@ -8,25 +8,29 @@ const {
 } = require('./token');
 
 /**
- * Authenticates a user's login credentials against the database.
+ * Authenticates a user's login credentials against the database. Provides a
+ * JSONWebToken authenticating the user's identity, valid for 30 days.
  * @param {string} usernameOrEmail The username or email address of the player.
  * @param {string} password The password of the user
- * @returns {ApiResponse} success: { status `200` }\
- * fail: status `401`, `500`
+ * @returns {ApiResponse} success: `200 OK` \
+ * fail: `400 BAD REQUEST`, `401 UNAUTHORIZED`, `500 INTERNAL SERVER ERROR`
  */
 const authenticateUserLogin = async (usernameOrEmail, password) => {
     // Make sure we have good input.
     const problems = [];
+
     if (typeof usernameOrEmail !== 'string') {
         problems.push('username is not a string.');
     } else if (!usernameOrEmail.length) {
         problems.push('username is empty.');
     }
+
     if (typeof password !== 'string') {
         problems.push('password is not a string.');
     } else if (!password.length) {
         problems.push('password is empty.');
     }
+
     if (problems.length) {
         return new ApiResponse(400, { problems });
     }
@@ -35,12 +39,12 @@ const authenticateUserLogin = async (usernameOrEmail, password) => {
     let users = [];
     try {
         users = await sql`
-        SELECT user_id, username, password, salt
-            FROM users
-            WHERE lower(username) = lower(${usernameOrEmail})
-                OR lower(email) = lower(${usernameOrEmail})
-            LIMIT 1;
-    `;
+            SELECT user_id, username, password, salt
+                FROM users
+                WHERE lower(username) = lower(${usernameOrEmail})
+                    OR lower(email) = lower(${usernameOrEmail})
+                LIMIT 1;
+        `;
     } catch (e) {
         return new ApiResponse(500, e);
     }
@@ -58,13 +62,29 @@ const authenticateUserLogin = async (usernameOrEmail, password) => {
         return new ApiResponse(401);
     }
 
+    // User has been authenticated. Generate them a new token.
     const { username, user_id } = user;
-
-    // The username and password match our records!
     const token = generateAuthenticationToken({ username, user_id });
     return new ApiResponse(200, { token });
 };
 
+/**
+ * Creates a new user in the database.
+ * @param {string} username The username of the user. Must follow the rules
+ * defined in `validator.js`.
+ * @param {string} email The email address of the user. Must follow the rules
+ * defined in `validator.js`.
+ * @param {string} password The password of the user. Must follow the rules
+ * defined in `validator.js`.
+ * @param {string|null} fName The first name of the user. This is not required,
+ * but is a good idea for professionals.
+ * @param {string|null} lName The last name of the user. This is not required,
+ * but is a good idea for professionals.
+ * @param {string|null} jobTitle The job title of the user (example: "Software
+ * Engineer"). This is not required, but is a good idea for professionals.
+ * @returns {ApiResponse} success: `201 CREATED`\
+ * fail: `400 BAD REQUEST` `500 INTERNAL SERVER ERROR`
+ */
 const createUser = async (
     username,
     email,
@@ -79,6 +99,8 @@ const createUser = async (
         delete validation.success;
         return new ApiResponse(400, validation);
     }
+
+    // Create db object.
     const userId = generateSalt(8);
     const salt = generateSalt();
     const hashedPw = hash(password, salt);
@@ -105,6 +127,13 @@ const createUser = async (
     }
 };
 
+/**
+ * This will retrieve the information for a single player, based on a token
+ * which should be provided by the `authenticateUserLogin` function.
+ * @param {string} token The JSON web token identifying the user.
+ * @returns {ApiResponse} Success: `200 OK`\
+ * Failure: `401 UNAUTHORIZED` `500 INTERNAL SERVER ERROR`
+ */
 const fetchUserByToken = async (token) => {
     const verification = verifyAuthenticationToken(token);
     if (!verification.success) {
