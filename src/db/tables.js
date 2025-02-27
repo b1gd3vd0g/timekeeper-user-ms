3d8189b02750e5705c933e0bd11d22c655183803
b1gd3vd0g <devin@bigdevdog.com>
2025-02-27 07:03:01 -0800

    Change user_id column to SERIAL

--- src/db/tables.js
+++ src/db/tables.js
@@ -3,12 +3,12 @@ const sql = require('./sql');
 const createUsersTable = async () => {
     await sql`
         CREATE TABLE IF NOT EXISTS users(
-            user_id     TEXT                        PRIMARY KEY UNIQUE NOT NULL,
+            user_id     SERIAL                      PRIMARY KEY,
             username    TEXT                        UNIQUE NOT NULL,
             email       TEXT                        UNIQUE NOT NULL,
             password    TEXT                        NOT NULL,   
             salt        TEXT                        NOT NULL,
-            created     TIMESTAMP WITH TIME ZONE    NOT NULL
+            created     TIMESTAMP WITH TIME ZONE    NOT NULL,
             f_name      TEXT,
             l_name      TEXT,
             job_title   TEXT
